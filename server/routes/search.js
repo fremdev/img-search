d121a32358afd8c1f57cac71370a77f7238011f4
Alex Efremov <alex.efremov.dev@gmail.com>
2016-11-10 21:00:55 +0200

    Add /latest route

--- server/routes/search.js
+++ server/routes/search.js
@@ -2,15 +2,24 @@ const express = require('express');
 const searchApi = express.Router();
 
 const getImages = require('../utils/imgur');
+const History = require('../models/history');
 
 searchApi.get('/search/:query', (req, res) => {
   getImages(req.params.query, req.query.offset).then((data) => {
+    new History({term: req.params.query}).save();
     res.json(data);
   });
 });
 
 searchApi.get('/latest', (req, res) => {
-  console.log('latest');
+  History
+    .find()
+    .select({_id: 0, term: 1, when: 1 })
+    .sort({when: -1})
+    .limit(10)
+    .then((data) => {
+      res.json(data);
+    });
 });
 
 module.exports = searchApi;
